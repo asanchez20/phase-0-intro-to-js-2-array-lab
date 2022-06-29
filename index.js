7ced144b134a36bf637337b4a5269eac17c2f74d
Alvin Sanchez <alvin.sanchez27@gmail.com>
2022-06-28 23:29:57 -0400

    First Commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const copyOfCat = [...cats, name]
+        return copyOfCat
+}
+function prependCat(name){
+    const copyOfCat = [name,...cats];
+        return copyOfCat;
+}
+function removeLastCat(){
+    const copyOfCat = cats.slice(0, 2);
+        return copyOfCat;
+}
+function removeFirstCat(){
+    const copyOfCat = cats.slice(1);
+        return copyOfCat;
+}
